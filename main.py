57ad056b9d2b308dc64e0d99705b28532b233828
gryph853 <gryphonseyrie@outlook.com>
2025-03-25 19:09:41 -0700

    still coping from solution

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 from stats import (
 get_num_words, 
 chars_dict_to_sorted_list,
 get_chars_dict
 )
 
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
